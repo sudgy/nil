2a5f6bc2b83cd708bce266ca2b1ee7392c6804a4
David Cohoe <sudgylacmoe@gmail.com>
2020-03-27 22:48:58 -0700

    Added two basic types of mov to sasm

--- elf/sasm/sasm.asm
+++ elf/sasm/sasm.asm
@@ -1,19 +1,21 @@
 ; SASM: The sudgy assembler.
 ; This version is so old that it can't even assemble itself.  It is not
 ; finished.  To make it easier to assemble itself, only the following commands
-; should be used to start with:
+; should be used in this file to start with:
 ;   syscall
 ;   mov (register), (immediate hex value)
 ;   mov (register), (register)
 ;   push (register)
 ;   pop (register)
 ;   jmp (label)
-;   cmp (register)
+;   cmp (register), (immediate hex value)
 ;   je (label)
 ;   jne (label)
+;   jl (label)
 ;   call (label)
 ;   ret
 ;   add (register, register)
+;   sub (register, register)
     pop rcx
     mov rdi, 0x1 ; In case we exit
     cmp rcx, 0x3
@@ -45,30 +47,24 @@
     ; Start the assembling
 line:
     call skipspac
-    call pushipos
     ; syscall
+    call pushipos
     mov rdx, 0x4 ; Can only cmp 32-bit values :(
     call readn
     cmp rax, 0x63737973 ; "sysc"
     je syscall_
     call popipos
+    ; mov
+    call pushipos
+    mov rdx, 0x3
+    call readn
+    cmp rax, 0x766F6D ; "mov"
+    je mov_
+    call popipos
     ; Nothing was found :(
 err:
     mov rdi, 0x4
     jmp exit
-syscall_:
-    mov rdx, 0x3
-    call readn
-    cmp rax, 0x6C6C61 ; "all"
-    jne err
-    mov rax, 0x050F ; syscall opcode
-    push rax
-    mov rax, 1
-    mov rdi, 4
-    mov rsi, rsp
-    mov rdx, 2
-    syscall
-    jmp skipcom
 ; Will store the byte in rax
 readchar:
     mov rax, 0x0 ; sys_read
@@ -91,18 +87,12 @@ readchar:
 skipcom:
     call readchar
     jmp skipcom
-; Skip until next space
+; Skip until next non-space
 skipspac:
     call readchar
     cmp rax, 0x20
     je skipspac
-    ; Go back one spot in the file so that the next read starts at the right
-    ; spot
-    mov rax, 0x8 ; sys_lseek
-    mov rdi, 0x3
-    mov rsi, 0xFFFFFFFFFFFFFFFF ; -1 :/
-    mov rdx, 0x1 ; SEEK_CUR
-    syscall
+    call iback
     ret
 ; Push the current position of the input file on the stack
 pushipos:
@@ -136,6 +126,160 @@ readn:
     syscall
     pop rax
     ret
+; Go back one character in the input file
+iback:
+    mov rax, 0x8 ; sys_lseek
+    mov rdi, 0x3
+    mov rdx, 0x1 ; SEEK_CUR
+    mov rsi, 0x0
+    sub rsi, rdx ; Set rsi to -1, rdx happens to be 1 already
+    syscall
+    ret
+; Write to the output file.  It assumes that the data is on the top of the stack
+; and that you have already set rdx.
+write:
+    pop rbx
+    mov rax, 0x1 ; sys_write
+    mov rdi, 0x4 ; output file
+    mov rsi, rsp
+    syscall
+    push rbx
+    ret
 exit:
     mov rax, 0x3C
     syscall
+return:
+    ret
+unreturn:
+    push rax
+    push rdx
+    call iback
+    pop rdx
+    pop rax
+    ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; INSTRUCTION READING HELPERS ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Read a register, storing it numerically in rax
+readreg:
+    mov rdx, 0x3
+    call readn
+    mov rdx, rax
+    cmp rdx, 0x786172 ; "rax"
+      mov rax, 0x0
+      je return
+    cmp rdx, 0x786372 ; "rcx"
+      mov rax, 0x1
+      je return
+    cmp rdx, 0x786472 ; "rdx"
+      mov rax, 0x2
+      je return
+    cmp rdx, 0x786272 ; "rbx"
+      mov rax, 0x3
+      je return
+    cmp rdx, 0x707372 ; "rsp"
+      mov rax, 0x4
+      je return
+    cmp rdx, 0x706272 ; "rbp"
+      mov rax, 0x5
+      je return
+    cmp rdx, 0x697372 ; "rsi"
+      mov rax, 0x6
+      je return
+    cmp rdx, 0x696472 ; "rdi"
+      mov rax, 0x7
+      je return
+    call err
+; Read a hex value into rdx.  Assumes that "0x" has already been parsed.
+; Currently, if the digits are not ended by a newline, space, or semicolon, the
+; behavior is undefined.
+readhex:
+    mov rdx, 0x0 ; Result
+  rdhexbeg:
+    push rdx
+    mov rdx, 0x1
+    call readn
+    pop rdx
+    cmp rax, 0xA ; newline
+    je unreturn
+    cmp rax, 0x20 ; space
+    je unreturn
+    cmp rax, 0x3B ; semicolon
+    je unreturn
+    mov rcx, 0x30 ; Convert ASCII digit to number
+    sub rax, rcx
+    cmp rax, 0xA ; Check if the digit is a letter
+    jl rdhexdig
+    mov rcx, 0x7 ; Convert ASCII Letter to number (already subtracted 0x30)
+    sub rax, rcx
+  rdhexdig:
+    ; Multiply rdx by 0x10
+    add rdx, rdx
+    add rdx, rdx
+    add rdx, rdx
+    add rdx, rdx
+    ; Combine values
+    add rdx, rax
+    jmp rdhexbeg
+;;;;;;;;;;;;;;;;;;;;;;;;;
+; SPECIFIC INSTRUCTIONS ;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+syscall_:
+    mov rdx, 0x3
+    call readn
+    cmp rax, 0x6C6C61 ; "all"
+    jne err
+    mov rax, 0x050F ; syscall opcode
+    push rax
+    mov rdx, 2
+    call write
+    pop rax
+    jmp skipcom
+mov_:
+    call skipspac
+    call readreg
+    push rax
+    call readchar ; For the ',', hope it's there.
+    call skipspac
+    call readchar ; Determine if it's a register or an immediate value
+    cmp rax, 0x30
+    jne mov_reg
+    ; Command is mov (register), (immediate hex value)
+    call readchar ; Hope that the next character is 'x'
+    call readhex ; rdx now has the immediate value
+    pop rax
+    push rdx
+    mov rdx, 0xB8 ; MOV opcode
+    add rax, rdx
+    push rax
+    mov rdx, 1
+    call write
+    pop rax
+    mov rdx, 4
+    call write
+    pop rdx
+    jmp skipcom
+  mov_reg:
+    ; Command is mov (register), (register)
+    call iback
+    call readreg
+    push rax
+    mov rax, 0x8B48 ; REW.W + MOV opcode
+    push rax
+    mov rdx, 2
+    call write
+    pop rax
+    ; Create ModRM Byte
+    pop rdx ; ModRM.rm
+    pop rax ; ModRM.reg
+    add rax, rax
+    add rax, rax
+    add rax, rax
+    add rax, rdx
+    mov rdx, 0xC0
+    add rax, rdx
+    push rax
+    mov rdx, 1
+    call write
+    pop rax
+    jmp skipcom
---
+++ elf/sasm/tests/0.0.2.asm
@@ -0,0 +1,4 @@
+mov rax, 0x3C
+mov rbx, 0x11
+mov rdi, rbx
+syscall
